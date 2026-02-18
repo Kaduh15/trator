c7dfb5ed894a1576630b64a62782d88857fe5e3b
Kadu <kadu.silva2014@gmail.com>
2026-02-18 07:03:15 -0300

    feat(auth): habilitar plugins no cliente web
    Habilita os plugins usernameClient e adminClient no auth client.\nMantem o front-end alinhado com as capacidades habilitadas no backend.

--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -1,6 +1,8 @@
-import { env } from "@trator/env/web";
-import { createAuthClient } from "better-auth/react";
+import { env } from '@trator/env/web'
+import { adminClient, usernameClient } from 'better-auth/client/plugins'
+import { createAuthClient } from 'better-auth/react'
 
 export const authClient = createAuthClient({
   baseURL: env.VITE_SERVER_URL,
-});
+  plugins: [usernameClient(), adminClient()],
+})
