e8f05c1412c88ac86b11a4cbbdd80a7334dd858a
Kadu <kadu.silva2014@gmail.com>
2026-03-05 17:19:37 -0300

    feat(web): criar telas de clientes e associacao
    Substitui a rota antiga por telas admin com detalhe e permite atualizar associacao via mutation.

--- apps/web/src/routes/(private)/clientes.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/(private)/clientes')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/(private)/clientes"!</div>
-}
